aaa69bf986edad5d1906b13a27d0ab4dc4990860
SanSynyster <sahab.taali@ymail.com>
2021-12-10 15:40:43 +0000

    changes made from theia lab environment

--- tamrin.py
+++ tamrin.py
@@ -1,4 +1,4 @@
 var1 = input("Enter variable 1:")
 var2 = input("Enter variable 2:")
 var1 , var2 = var2 , var1
-print("Variable 1 is " + var1 ,"Variable 2 is " + var2  )
+print("Variable 1 is " + var1 ,"Variable 2 is " + var2 , "this is a test for cloning a repository!" )
